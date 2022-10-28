65aa473e51e0fd3ab85f16c37e2af78ba7cbf7c9
NicolasVidal-IsCool <nicolas.vidal@iscool-e.Com>
2022-10-28 19:00:36 +0200

    Shuffle Positions is working ! :)

--- Assets/MainScript.cs
+++ Assets/MainScript.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
+// class A
+// {
+//     public void Pouet()
+//     {
+//         var instance = new MainScript();
+//
+//         var method = instance.GetType().GetMethod("Start", BindingFlags.Instance | BindingFlags.NonPublic);
+//
+//         method.Invoke(instance, null);
+//     }
+// }
+
 
 public class MainScript : MonoBehaviour
 {
+    public Transform[] cubesTransforms;
+    
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        Debug.Log("Hello les 3A IABD 2 ! :) :) :)");
         
+        Debug.Log($"Il y a {cubesTransforms.Length} composants dans mon tableau :)");
+
+        ShufflePositions(cubesTransforms);
     }
 
-    // Update is called once per frame
-    void Update()
+    private static void ShufflePositions(Transform[] cubes)
     {
-        
+        for (var i = 0; i < cubes.Length; i++)
+        {
+            var rdmIdx = Random.Range(i, cubes.Length);
+            
+            (cubes[i].position, cubes[rdmIdx].position) = (cubes[rdmIdx].position, cubes[i].position);
+        }
     }
 }
